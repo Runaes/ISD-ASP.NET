d087b5397cb89537774e581d55be24156d5d03b8
YasinMC <99112486@student.uts.edu.au>
2019-05-30 16:41:35 +1000

    Partial implementation of orders page

---
+++ MvcMovie/Data/dbo.Order.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Order] (
+    [ID]     INT        NOT NULL,
+    [UserID]          NCHAR (10) NULL,
+    [OrderListID] NCHAR (10) NULL,
+    [Payment] BIT NULL, 
+    [PaymentAmt] FLOAT NULL, 
+    [PurchaseDate] DATE NULL, 
+    [PaymentType] NCHAR(10) NULL, 
+    PRIMARY KEY CLUSTERED ([ID])
+);
+
--- MvcMovie/wwwroot/js/site.js
+++ MvcMovie/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(function () {
+    var $select = $(".1-100");
+    for (i = 1; i <= 10; i++) {
+        $select.append($('<option></option>').val(i).html(i))
+    }
+});
